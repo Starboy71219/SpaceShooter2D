caa361dfb3202082e8307e3e2f4ff4e6ac9e3228
Ritesh Kumar <82120977+Starboy71219@users.noreply.github.com>
2022-07-03 01:47:36 +0530

    file upload

---
+++ Assets/playermove.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermove : MonoBehaviour
+{
+    float maxSpeed = 5f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.y += Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime;
+        transform.position = pos;
+
+    }
+}
